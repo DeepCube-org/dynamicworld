2ea4706ff8f6cf4cd8c4377872d74443feb1e6b7
Federico <federico.ricciuti@tre-altamira.com>
2023-05-31 09:39:52 +0200

    Making the benchmark indipendent

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -9,7 +9,7 @@ import numpy as np
 try:
     from tensorflow.test.experimental import sync_devices
     print('TensorFlow version supporting sync_devices')
-    
+
 except ModuleNotFoundError:
     from experimental import sync_devices
     print('TensorFlow version not supporting sync_devices')
@@ -19,16 +19,7 @@ from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.saved_model import signature_constants
 
 
-os.system('python --version')
-print(tf.__version__)
 
-physical_devices = tf.config.list_physical_devices('GPU')
-assert len(physical_devices) > 0, 'No GPUs available'
-tf.config.set_visible_devices(physical_devices[0], 'GPU') #Only the first GPU will be considered
-
-
-RESOLUTION = 224
-CHANNELS = 9
 
 def get_dummy(shape):
     x = tf.random.uniform(shape)
@@ -46,14 +37,14 @@ def time_model(model, dummy_input):
     Get the time spent for the inference, measured in ms
     """
     with tf.device('/GPU:0'):
-        
+
         sync_devices()
         starter = time.time()*1000.0
         _ = model(dummy_input)
         sync_devices()
         ender = time.time()*1000.0
         curr_time = ender-starter
-    
+
     return(curr_time)
 
 
@@ -61,7 +52,7 @@ def warm_up(model, repetitions=50):
     dummy_input = get_dummy((1, RESOLUTION, RESOLUTION, CHANNELS))
     for _ in range(repetitions):
         _ = time_model(model, dummy_input)
-        
+
 
 def get_optimal_resolution(model):
     warm_up(model)
@@ -78,15 +69,15 @@ def get_optimal_resolution(model):
 
 def get_latency(model, resolution):
     warm_up(model)
-    
+
     repetitions = 300
     timings=np.zeros((repetitions,1))
-    
+
     # MEASURE PERFORMANCE
     for rep in range(repetitions):
         dummy_input = get_dummy((1, resolution, resolution, CHANNELS))
         timings[rep] = time_model(model, dummy_input)
-        
+
     mean_syn = np.sum(timings) / repetitions
     std_syn = np.std(timings)    
     return mean_syn, std_syn
@@ -94,7 +85,7 @@ def get_latency(model, resolution):
 
 def get_optimal_batch_size(model):
     warm_up(model)
-    
+
     optimal_batch_size = 1
     for batch_size in [32, 64, 128, 256, 512, 1024]:
         dummy_input = get_dummy((batch_size, RESOLUTION, RESOLUTION, CHANNELS))
@@ -109,30 +100,45 @@ def get_optimal_batch_size(model):
 
 def get_throughput(model, batch_size, resolution):
     warm_up(model)
-    
+
     repetitions = 100
     total_time  = 0
     for rep in range(repetitions):
         dummy_input = get_dummy((batch_size, resolution, resolution, CHANNELS))
         total_time += time_model(model, dummy_input)/1000 #to convert in second (original in ms)
-    
+
     throughput =   (repetitions*batch_size)/total_time  # n_images/total_time 
     return(throughput)
 
 
 
-model, _ = get_func_from_saved_model('dynamicworld/model/model/forward_trt/')
+if __name__ == '__main__':
 
-for i in range(2):
-    mean, std = get_latency(model, RESOLUTION)
 
-print('Latency, average time (ms):', mean)
-print('Latency, std time (ms):', std)
-    
-#optimal_batch_size = get_optimal_batch_size(model)
-optimal_batch_size = 64
-for i in range(2):
-    throughput = get_throughput(model, optimal_batch_size, RESOLUTION)
+    os.system('python --version')
+    print(tf.__version__)
 
-print('Optimal Batch Size:', optimal_batch_size)
-print('Final Throughput (imgs/s):',throughput)
+    physical_devices = tf.config.list_physical_devices('GPU')
+    assert len(physical_devices) > 0, 'No GPUs available'
+    tf.config.set_visible_devices(physical_devices[0], 'GPU') #Only the first GPU will be considered
+
+
+    RESOLUTION = 224
+    CHANNELS = 9
+
+
+    model, _ = get_func_from_saved_model('dynamicworld/model/model/forward_trt/')
+
+    for i in range(2):
+        mean, std = get_latency(model, RESOLUTION)
+
+    print('Latency, average time (ms):', mean)
+    print('Latency, std time (ms):', std)
+
+    #optimal_batch_size = get_optimal_batch_size(model)
+    optimal_batch_size = 64
+    for i in range(2):
+        throughput = get_throughput(model, optimal_batch_size, RESOLUTION)
+
+    print('Optimal Batch Size:', optimal_batch_size)
+    print('Final Throughput (imgs/s):',throughput)
--- benchmark/tf2rt.py
+++ benchmark/tf2rt.py
@@ -10,49 +10,48 @@ import tensorflow as tf
 import tensorflow as tf
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
  
+if __name__ == '__main__':
+
+    saved_model_dir = 'dynamicworld/model/model/forward/'
+    output_saved_model_dir = 'dynamicworld/model/model/forward_trt/'
+
+    # Instantiate the TF-TRT converter
+    converter = trt.TrtGraphConverterV2(
+       input_saved_model_dir=saved_model_dir,
+       precision_mode=trt.TrtPrecisionMode.FP32,
+       use_calibration=False,
+       use_dynamic_shape=True, # Enable dynamic shape for all the dimensions
+       dynamic_shape_profile_strategy='Optimal', # Limited by the inputs provided during the build but the best performing one
+       maximum_cached_engines = 16, 
+       allow_build_at_runtime = True
+    )
+
+    # Convert the model into TRT compatible segments
+    trt_func = converter.convert()
+    converter.summary()
 
 
-saved_model_dir = '../dynamicworld/model/model/forward/'
-output_saved_model_dir = '../dynamicworld/model/model/forward_trt/'
+    def input_fn():
+        # max batch size expected to be used in inference, 
+        #If we try to infer the model with larger batch size, then TF-TRT will build another engine to do so.
+        xs = [
+            #tf.ones((64, 256, 256, 9), tf.float32),
+            #tf.ones((32, 256, 256, 9), tf.float32),
+            #tf.ones((1,  256, 256, 9), tf.float32),
 
 
-# Instantiate the TF-TRT converter
-converter = trt.TrtGraphConverterV2(
-   input_saved_model_dir=saved_model_dir,
-   precision_mode=trt.TrtPrecisionMode.FP32,
-   use_calibration=False,
-   use_dynamic_shape=True, # Enable dynamic shape for all the dimensions
-   dynamic_shape_profile_strategy='Optimal', # Limited by the inputs provided during the build but the best performing one
-   maximum_cached_engines = 16, 
-   allow_build_at_runtime = True
-)
- 
-# Convert the model into TRT compatible segments
-trt_func = converter.convert()
-converter.summary()
+            #tf.ones((128, 224, 224, 9), tf.float32),
+            tf.ones((64, 224, 224, 9), tf.float32),
+            tf.ones((32, 224, 224, 9), tf.float32),
+            tf.ones((1,  224, 224, 9), tf.float32),
 
+            #tf.ones((128, 128, 128, 9), tf.float32),
+            tf.ones((64, 128, 128, 9), tf.float32),
+            tf.ones((32, 128, 128, 9), tf.float32),
+            tf.ones((1,  128, 128, 9), tf.float32)
+        ]
+        for x in xs:
+            yield [x]
 
-def input_fn():
-    # max batch size expected to be used in inference, 
-    #If we try to infer the model with larger batch size, then TF-TRT will build another engine to do so.
-    xs = [
-        #tf.ones((64, 256, 256, 9), tf.float32),
-        #tf.ones((32, 256, 256, 9), tf.float32),
-        #tf.ones((1,  256, 256, 9), tf.float32),
-        
-        
-        tf.ones((128, 224, 224, 9), tf.float32),
-        tf.ones((64, 224, 224, 9), tf.float32),
-        tf.ones((32, 224, 224, 9), tf.float32),
-        tf.ones((1,  224, 224, 9), tf.float32),
-        
-        tf.ones((128, 128, 128, 9), tf.float32),
-        tf.ones((64, 128, 128, 9), tf.float32),
-        tf.ones((32, 128, 128, 9), tf.float32),
-        tf.ones((1,  128, 128, 9), tf.float32)
-    ]
-    for x in xs:
-        yield [x]
-
-converter.build(input_fn=input_fn)
-converter.save(output_saved_model_dir=output_saved_model_dir)
+    converter.build(input_fn=input_fn)
+    converter.save(output_saved_model_dir=output_saved_model_dir)
